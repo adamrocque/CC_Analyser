ae0d28bd6936c2fcce1eed642f632d7656d2c35f
adamrocque <55038209+adamrocque@users.noreply.github.com>
2019-09-09 16:27:29 -0400

    Updated filter for transaction - UGLY:(
    Updated the way we pre-process the transaction name so that its easier to determine what we're looking for. This is currently done really ugly, so it needs to change. But I think first, I want to correctly append to a list of unknown entries, and then also update to use csv tables for the transaction lists. Perhaps keep the same dictionary properties, just read in the csv files into the separate dictionary lists.

--- csv_analyzer.py
+++ csv_analyzer.py
@@ -7,12 +7,8 @@ import string
 import sys
 import os
 import datetime
-
-#from evertz.mediator.ws import MediatorHttpClientV1
-
 import csv
 import shutil
-import datetime
 from tempfile import NamedTemporaryFile
 
 ##Lists of Categories
@@ -28,13 +24,21 @@ amazon = {
     "total": 0,
     "list":['AMZN','AMAZON'],
 }
-amzn_list = ['AMZN','AMAZON']
+#amzn_list = ['AMZN','AMAZON']
 
 #Bar
-bar_list = ['BLACK DOG']
+bar = {
+    "total": 0,
+    "list":['BLACK DOG',"LOOKOUT SPORTS LOUNGE"],
+}
+#bar_list = ['BLACK DOG']
 
 #CarPayment
-car_pay_list = ['HONDA']
+car_pay = {
+    "total": 0,
+    "list":['HONDA'],
+}
+#car_pay_list = ['HONDA']
 
 ##CC_Payment
 #cc_payment_list = ['PAYMENT']
@@ -48,27 +52,68 @@ food = {
 #food_list = ['OSMOWS',"MCDONALD'S","WENDY'S","TIM HORTONS"]
 
 #Fun
-fun_list = ['CONFUNDRUM','BEST BUY','BARANGA']
+fun = {
+    "total": 0,
+    "list":['CONFUNDRUM','BEST BUY','BARANGA', "THE MULE INC"],
+}
+
+#fun_list= ['CONFUNDRUM','BEST BUY','BARANGA']
 
 #Gas
-gas_list = ['PETROCAN']
+gas = {
+    "total": 0,
+    "list":['PETROCAN'],
+}
+
+#gas_list = ['PETROCAN']
 
 #Groc
-groc_list = ['SOBEYS','FORTINOS','LOBLAWS','FARM BOY','FRESHCO']
+groc = {
+    "total": 0,
+    "list":['SOBEYS','FORTINOS','LOBLAWS','FARM BOY','FRESHCO', "ASLAN"],
+}
+
+#groc_list = ['SOBEYS','FORTINOS','LOBLAWS','FARM BOY','FRESHCO']
 
 #Gym
-gym_list = ['GOODLIFE']
+gym = {
+    "total": 0,
+    "list":['GOODLIFE'],
+}
+
+#gym_list = ['GOODLIFE']
 
 #House
-house_list = ['HOME DEPOT','EQUIFAX','WAL-MART','ZEHRS']
+house  = {
+    "total": 0,
+    "list":['HOME DEPOT','EQUIFAX','WAL-MART','ZEHRS',"BELL CANADA", "CDN TIRE"],
+}
+#house_list = ['HOME DEPOT','EQUIFAX','WAL-MART','ZEHRS']
 
 #Insurance
-insur_list = ['INSURANCE']
+insur  = {
+    "total": 0,
+    "list":['INSURANCE'],
+}
+
+#insur_list = ['INSURANCE']
 
 #Paypal
-paypal_list = ['PAYPAL']
+paypal  = {
+    "total": 0,
+    "list":['PAYPAL'],
+}
+
+#paypal_list = ['PAYPAL']
 
 #Travel
+travel  = {
+    "total": 0,
+    "list":['PARKING',"PRESTO"],
+}
+#Unknown Entries
+unknown = {}
+
 
 def get_length(file_path):
     with open("data.csv") as csvfile:
@@ -93,6 +138,13 @@ def append_data(file_path, name, email, amount):
             "date": datetime.datetime.now(),            
             })
 
+def find_nth(haystack, needle, n):
+    start = haystack.find(needle)
+    while start >= 0 and n > 1:
+        start = haystack.find(needle, start+len(needle))
+        n -= 1
+    return start
+
 def enter_catergory(row,count):
     print(row['Transaction'])
     transaction = row['Transaction']
@@ -102,18 +154,121 @@ def enter_catergory(row,count):
         if transaction.index("#") >= 0:
             transaction = transaction[0:transaction.index("#") -1]
     except ValueError:
-        print("This transaction has no # signs in it")
+        # print("This transaction has no # signs in it")
+        transaction = transaction
 
-    print("Checking if {0} is in the food list".format(transaction))
+    try:
+        if transaction.upper().index("THE") == 0:
+            # print("This transaction starts with a THE, omitting it")
+            transaction = transaction[3:]
+    except ValueError:
+        transaction = transaction
+        # print("This transaction does not start with a 'THE' ")
+
+    try:
+        if '/' in transaction:
+            transaction = transaction[0:transaction.index("/") ]  
+    except ValueError:
+        transaction = transaction      
+        # print("This transaction has no / signs in it")
+
+    try:
+        if '(' in transaction:
+            transaction = transaction[0:transaction.index("(") -1]  
+    except ValueError:
+        transaction = transaction
+
+    try:
+        if '-' in transaction:
+            try:
+                if transaction.index("WAL-MART") > 0:
+                    print("Found a dash, but its  WAL-MART, using WAL-MART")
+                    transaction = transaction[0:transaction.index(" ")] 
+
+            except:
+                    print("Found a dash, but not WAL-MART, using transaction before -")
+                    transaction = transaction[0:transaction.index("-")] 
+    except ValueError:
+        print("Defaulting here?")
+        transaction = transaction
+
+
+    try:
+        spc_index = find_nth(transaction, " ", 1)
+        if spc_index >= 0:
+            transaction = transaction[0:spc_index]
+        else:
+            transaction = transaction[0:10]
+    except ValueError:
+        transaction = transaction
+        # print("This transaction has fewer than 1 space in it, but it was also shorter than 10 chars")
+
+
+    print("Checking if {0} is in any lists".format(transaction))
     #if any(transaction in s for s in food_list):
-    if any(transaction[0:10] in s for s in food["list"]):
-        print("\nWe found a transaction [{0}] that should be catergorized as an Food purchase".format(transaction))
+    if any(transaction in s for s in food["list"]):
+        print("\nWe found a transaction [{0}] that should be catergorized as an FOOD purchase".format(transaction))
         print("Adding [{0}] to the food total\n".format(float(row["Debit"])))
         food["total"] += float(row["Debit"])
         #print(row['Transaction'])
 
+    elif any(transaction in s for s in groc["list"]):
+        print("\nWe found a transaction [{0}] that should be catergorized as an GROC purchase".format(transaction))
+        print("Adding [{0}] to the food total\n".format(float(row["Debit"])))
+        groc["total"] += float(row["Debit"])
+        #print(row['Transaction'])
+
+
+    elif any(transaction in s for s in alc["list"]):
+        print("\nWe found a transaction [{0}] that should be catergorized as an ALC purchase".format(transaction))
+        print("Adding [{0}] to the food total\n".format(float(row["Debit"])))
+        alc["total"] += float(row["Debit"])
+        #print(row['Transaction'])
+
+
+    elif any(transaction in s for s in house["list"]):
+        print("\nWe found a transaction [{0}] that should be catergorized as an HOUSE purchase".format(transaction))
+        print("Adding [{0}] to the food total\n".format(float(row["Debit"])))
+        house["total"] += float(row["Debit"])
+        #print(row['Transaction'])
+
+    elif any(transaction in s for s in travel["list"]):
+        print("\nWe found a transaction [{0}] that should be catergorized as an HOUSE purchase".format(transaction))
+        print("Adding [{0}] to the food total\n".format(float(row["Debit"])))
+        travel["total"] += float(row["Debit"])
+        #print(row['Transaction'])        
+
+    elif any(transaction in s for s in gas["list"]):
+        print("\nWe found a transaction [{0}] that should be catergorized as an GAS purchase".format(transaction))
+        print("Adding [{0}] to the food total\n".format(float(row["Debit"])))
+        gas["total"] += float(row["Debit"])
+        #print(row['Transaction'])
+
+    elif any(transaction in s for s in bar["list"]):
+        print("\nWe found a transaction [{0}] that should be catergorized as an GAS purchase".format(transaction))
+        print("Adding [{0}] to the food total\n".format(float(row["Debit"])))
+        bar["total"] += float(row["Debit"])
+        #print(row['Transaction'])  
+
+    elif any(transaction in s for s in amazon["list"]):
+        print("\nWe found a transaction [{0}] that should be catergorized as an GAS purchase".format(transaction))
+        print("Adding [{0}] to the food total\n".format(float(row["Debit"])))
+        amazon["total"] += float(row["Debit"])
+        #print(row['Transaction'])                
+
+    elif any(transaction in s for s in paypal["list"]):
+        print("\nWe found a transaction [{0}] that should be catergorized as an GAS purchase".format(transaction))
+        print("Adding [{0}] to the food total\n".format(float(row["Debit"])))
+        paypal["total"] += float(row["Debit"])
+        #print(row['Transaction'])        
+
     else:
-        print("Nope, not in the food list")
+        print("Nope, not in any of the lists, add to the unknown list")
+        # unknown["total"] += float(row["Debit"])
+        # unknown["list"].append(transaction)
+        print("An Unknown Transaction: {0}, Its Value: {1} ".format(transaction,row["Debit"]))
+        unknown[transaction] = row["Debit"]
+
 #append_data("data.csv", "Adam", "adam@evertz.com",123.22)
 
 
@@ -136,7 +291,18 @@ with open(filename, "rb") as csvfile, temp_file:
     for row in reader:
         enter_catergory(row,count)
 
-print("We found this total spent in food: {0}".format(food_dict))
+#print("We found this total spent in food: {0}".format(food_dict))
+print("We found this total spent in FOOD: {0}".format(food["total"]))
+print("We found this total spent in ALC: {0}".format(alc["total"]))
+print("We found this total spent in HOUSE: {0}".format(house["total"]))
+print("We found this total spent in GAS: {0}".format(gas["total"]))
+print("We found this total spent in GROC: {0}".format(groc["total"]))
+print("We found this total spent in PAYPAL: {0}".format(paypal["total"]))
+
+print("The transactions we couldn't understand: ")
+for transac in unknown:
+    print("Transaction: {0}  Value: {1}".format(transac, unknown[transac]))
+
 
 #         ##if row['Transaction']
 
